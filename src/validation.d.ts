c5dde2c8c60fac4c40ff5030a709c048548cbcba
andy.patterson <andy.patterson@genesys.com>
2018-04-08 13:26:25 -0400

    chore: clean up conditional types

--- src/validation.d.ts
+++ src/validation.d.ts
@@ -1,4 +1,4 @@
-import { Required } from 'simplytyped';
+import { Required, UnionizeTuple } from 'simplytyped';
 import { StringSchema } from './defs/string';
 import { NumberSchema } from './defs/number';
 import { BooleanSchema } from './defs/boolean';
@@ -8,23 +8,28 @@ import { ObjectSchema } from './defs/object';
 import { EnumSchema, AllOf, AnyOf, OneOf, Not, Ref } from './defs/other';
 import { SchemaDefinition } from './index';
 
-type SchematizeProperties<S extends ObjectSchema> = S['properties'] extends Record<string, SchemaDefinition> ? {
-    [K in keyof S['properties']]?: Schematize1<S['properties'][K]>;
-} : object;
+type SchematizeProperties<S extends ObjectSchema> = S['properties'] extends Record<string, SchemaDefinition>
+    ? { [K in keyof S['properties']]?: Schematize1<S['properties'][K]> }
+    : object;
 
-type MarkRequired<S extends ObjectSchema, P extends object> = S['required'] extends any[] ?
-    Required<P, S['required'][number]> : P;
+type MarkRequired<S extends ObjectSchema, P extends object> = S['required'] extends any[]
+    ? Required<P, UnionizeTuple<S['required']>>
+    : P;
 
 // validate object typed schemas
 type SchematizeObject<S extends ObjectSchema> = MarkRequired<S, SchematizeProperties<S>>;
 
 // validate array typed schemas
 type SchematizeItems<S extends TupleSchema | ArraySchema> = {
-    [K: number]: S['items'] extends SchemaDefinition ? Schematize1<S['items']> : any;
+    [K: number]: S['items'] extends SchemaDefinition
+        ? Schematize1<S['items']>
+        : any;
 }
 
 // validate string typed schemas
-type SchematizeString<S extends StringSchema> = S['enum'] extends string[] ? S['enum'][number] : string;
+type SchematizeString<S extends StringSchema> = S['enum'] extends string[]
+    ? UnionizeTuple<S['enum']>
+    : string;
 
 // this duplicates Schematize to allow arbitrary recursion depth without TS complaining about circular references
 type Schematize1<S extends SchemaDefinition> =
